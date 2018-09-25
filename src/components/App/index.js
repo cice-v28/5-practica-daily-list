4f6b0fdcabd1c40daa240d70e2b355e7d431845f
Josu <josujubera@hotmail.com>
2018-09-25 21:46:44 +0200

    Added save button and upload button #time 10m
    Signed-off-by: Josu <josujubera@hotmail.com>

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,8 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import PlaylistAdd from "@material-ui/icons/PlaylistAdd";
+import CloudUpload from "@material-ui/icons/CloudUpload";
+import CloudDownload from "@material-ui/icons/CloudDownload";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
@@ -39,44 +41,44 @@ const DialogListCreator = ({
   onCreateList,
   onChangeCreatorText
 }) => (
-  <Dialog
-    open={isOpen}
-    onClose={onHandleClose}
-    aria-labelledby="form-dialog-title"
-  >
-    <DialogTitle id="form-dialog-title">Crear nueva lista</DialogTitle>
-    <DialogContent>
-      <DialogContentText>
-        Introduce el nombre de la lista que quieres crear.
+    <Dialog
+      open={isOpen}
+      onClose={onHandleClose}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle id="form-dialog-title">Crear nueva lista</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Introduce el nombre de la lista que quieres crear.
       </DialogContentText>
-      <TextField
-        autoFocus
-        margin="dense"
-        id="listname"
-        label="Nombre de lista"
-        type="text"
-        value={valueCreator}
-        onChange={e => onChangeCreatorText(e.target.value)}
-        fullWidth
-      />
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={onHandleClose} color="primary">
-        Cancel
+        <TextField
+          autoFocus
+          margin="dense"
+          id="listname"
+          label="Nombre de lista"
+          type="text"
+          value={valueCreator}
+          onChange={e => onChangeCreatorText(e.target.value)}
+          fullWidth
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onHandleClose} color="primary">
+          Cancel
       </Button>
-      <Button onClick={onCreateList} color="secondary">
-        Crear
+        <Button onClick={onCreateList} color="secondary">
+          Crear
       </Button>
-    </DialogActions>
-  </Dialog>
-);
+      </DialogActions>
+    </Dialog>
+  );
 
 const MasterList = ({ isOpen, onClose, items, onSelectList }) => (
   <Drawer open={isOpen} onClose={onClose}>
     <div tabIndex={0} role="button" onClick={onClose} onKeyDown={onClose}>
       <List component="nav">
-        {items.map(item => (
-          <ListItem button onClick={() => onSelectList(item.id)}>
+        {items.map((item, index) => (
+          <ListItem button key={index} onClick={() => onSelectList(item.id)}>
             <ListItemText primary={item.name} />
           </ListItem>
         ))}
@@ -100,6 +102,14 @@ class App extends React.Component {
 
   handleOpenDialog() {
     this.setState({ dialogOpen: true });
+  }
+
+  saveLists() {
+    window.localStorage.setItem('lists', JSON.stringify(this.state.lists));
+  }
+
+  loadLists() {
+    this.setState({ lists: JSON.parse(window.localStorage.getItem('lists')) });
   }
 
   createList() {
@@ -179,6 +189,20 @@ class App extends React.Component {
                   onClick={() => this.handleOpenDialog()}
                 >
                   <PlaylistAdd />
+                </IconButton>
+                <IconButton
+                  aria-label="Crear lista"
+                  style={{ "color": "white" }}
+                  onClick={() => this.loadLists()}
+                >
+                  <CloudUpload />
+                </IconButton>
+                <IconButton
+                  aria-label="Crear lista"
+                  style={{ "color": "white" }}
+                  onClick={() => this.saveLists()}
+                >
+                  <CloudDownload />
                 </IconButton>
               </Toolbar>
             </AppBar>
